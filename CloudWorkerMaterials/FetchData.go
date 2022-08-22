6a416a9fc6b8ca2005c1e692de9919ebb2bc3bf3
root <root@ip-172-31-10-73.us-west-2.compute.internal>
2022-08-22 21:17:08 +0000

    Cloud worker materials added.

---
+++ CloudWorkerMaterials/FetchData.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"os/exec"
+	"time"
+)
+
+func main() {
+	app := "aws"
+
+	arg0 := "s3"
+	arg1 := "cp"
+	arg2 := "s3://fw-yuanjun-test/edge_data.txt"
+	arg3 := "edge_data.txt"
+
+	for {
+		cmd := exec.Command(app, arg0, arg1, arg2, arg3)
+		_, err := cmd.Output()
+
+		if err != nil {
+			fmt.Println(err.Error())
+			return
+		}
+
+		cmd2 := exec.Command("echo", "Fetching the data from cloud storage......")
+		stdout2, err := cmd2.Output()
+
+		if err != nil {
+			fmt.Println(err.Error())
+			return
+		}
+
+		// Print the output
+		fmt.Println(string(stdout2))
+
+		time.Sleep(time.Second * 10)
+	}
+}
