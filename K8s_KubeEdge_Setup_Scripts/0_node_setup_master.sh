930a0489fecf2e7c28414462da89ebe5a132b749
Peng Du <rick.peng.du@gmail.com>
2022-08-24 14:53:38 -0400

    Update 0_node_setup_master.sh

--- K8s_KubeEdge_Setup_Scripts/0_node_setup_master.sh
+++ K8s_KubeEdge_Setup_Scripts/0_node_setup_master.sh
@@ -21,7 +21,7 @@ curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.
 echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
 
 apt-get update
-apt-get install -y kubelet kubeadm kubectl
+apt-get install -y kubelet=1.24.0-00 kubeadm=1.24.0-00 kubectl=1.24.0-00
 apt-mark hold kubelet kubeadm kubectl
 
 apt -y install docker.io
